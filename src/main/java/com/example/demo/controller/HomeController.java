422cf572876e68bbf49b4dcda1607bf8ba93ce93
kmusicsports <naokik.musicsports1027@gmail.com>
2023-04-21 10:36:26 +0900

    remove recruitmentRepository and recruitmentList from showHome method in HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -8,10 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.entity.Post;
-import com.example.demo.entity.Recruitment;
-import com.example.demo.form.PostQuery;
 import com.example.demo.repository.PostRepository;
-import com.example.demo.repository.RecruitmentRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -21,17 +17,14 @@ import lombok.AllArgsConstructor;
 public class HomeController {
 
     private final PostRepository postRepository;
-    private final RecruitmentRepository recruitmentRepository;
 
     @GetMapping("/")
     public ModelAndView showHome(ModelAndView mav) {
 
         List<Post> postList = postRepository.findTop3ByOrderByIdDesc();
-        List<Recruitment> recruitmentList = recruitmentRepository.findTop3ByDeadlineAfterOrderByDeadlineAsc(new Date());
 
         mav.setViewName("home");
         mav.addObject("postList", postList);
-        mav.addObject("recruitmentList", recruitmentList);
 
         return mav;
     }
